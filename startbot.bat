0536f718739795527d3fcd4f6661181a08f0bc8b
jakobkg <j@kob.codes>
2021-02-16 12:30:14 +0100

    check if docker compose is available before launch

--- startbot.bat
+++ startbot.bat
@@ -2,4 +2,10 @@
 Rem Start the Docker containers after building them fresh
 Rem --abort-on-container-exit makes it so if one container exits, they both quit as gracefully as possible
 Rem This is to clean up in case the Discord client stops due to a missing/invalid token, or if there's a database issue
-docker-compose up --build --abort-on-container-exit
+
+WHERE docker-compose>nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Docker Compose not installed! Exiting...
+) ELSE (
+  docker-compose up --build --abort-on-container-exit
+)
